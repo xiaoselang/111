ac3222f2ad46a3b1a666b7fe46ae9d666d952e2c
xiaoselang <1078645241@qq.com>
2017-12-07 23:02:39 +0800

    day01

--- js/index.js
+++ js/index.js
@@ -1,3 +1,64 @@
 /**
  * Created by Administrator on 2017\12\6 0006.
  */
+$(function () {
+    $.ajax({
+        url:"php/receipt.json",
+    }).done(function(result){
+        var oUl=$('<ul></ul>');
+        for(var key in result){
+            var oLi=$('<li></li>');
+            var oP=$('<p></p>');
+            oP.addClass('add1');
+            oP.html(key);
+            oLi.append(oP);;
+
+            for(var k in result[key]){
+                var oSpan=$('<span></span>');
+                oSpan.html(result[key][k]);
+                oSpan.addClass('add2');
+                oLi.append(oSpan);
+            }
+            oLi.addClass('add3')
+            oUl.append(oLi);
+
+        }
+        oUl.css({'position':'absolute','left':'50px','top':'30px','display':'none'})
+        $('.address').append(oUl)
+    })
+    $('.address').hover(function(){
+        $(this).children('ul').show();
+        $(this).css('background','#fff');
+        console.log($('.add2'));
+        $('.add2').on('click',function(){
+            console.log($('.address').children('li').eq(1));
+            console.log($(this).html());
+            $('.address').children('li').eq(1).html($(this).html());
+        })
+    },function(){
+        $(this).children('ul').hide();
+        $(this).css('background','#f1f1f1')
+    });
+    $('.login>li>a').hover(function(){
+        $(this).css({'text-decoration':'underline','color':'#157cdb'});
+    },function(){
+        $(this).css({'text-decoration':'none','color':'#999'});
+    });
+    $('#login').hover(function(){
+        $(this).css('text-decoration','underline');
+    },function(){
+        $(this).css('text-decoration','none');
+    });
+    $('#login').on('click',function(){
+        window.open('login.html');
+    });
+    $('#register').on('click',function(){
+        window.open('register.html');
+    })
+    $('.mine').hover(function(){
+        $(this).find('ul').show();
+    },function(){
+        $(this).find('ul').hide();
+    })
+
+})
--- js/login.js
+++ js/login.js
@@ -1,3 +1,13 @@
 /**
  * Created by Administrator on 2017\12\6 0006.
  */
+$(function(){
+    $('.tab').find('li').eq(0).css({'color':'#2C9EE1','border-bottom':'2px solid #2C9EE1'});
+    $('.tab li').on('click',function(){
+        $(this).css({'color':'#2C9EE1','border-bottom':'2px solid #2C9EE1'}).siblings('li').css({'color':'#333','border-bottom':'0'});
+        $('.tab').siblings('div').eq($(this).index()).show().siblings('div').hide();
+    });
+    function Normal() {
+
+    };
+})
